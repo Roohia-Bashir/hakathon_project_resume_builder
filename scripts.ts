df2240835bda6352995f45017082bc74530ba49c
Roohia-Bashir <160015350+Roohia-Bashir@users.noreply.github.com>
2024-09-25 16:32:44 +0500

    Delete scripts.ts

--- scripts.ts
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('resumeForm') as HTMLFormElement;
-const generateResumeBtn = document.getElementById('generateResume');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    window.open('./Preview_Resume/ynamicResume.html')
-    
-})
