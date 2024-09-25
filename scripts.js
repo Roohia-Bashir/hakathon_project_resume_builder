a4754f3b26a14bb94230ffe362e0c592a42bfb04
Roohia-Bashir <160015350+Roohia-Bashir@users.noreply.github.com>
2024-09-25 16:32:34 +0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const form = document.getElementById('resumeForm');
-const generateResumeBtn = document.getElementById('generateResume');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    window.open('./Preview_Resume/ynamicResume.html');
-});
